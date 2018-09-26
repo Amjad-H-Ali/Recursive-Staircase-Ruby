7834c2500e6e7d545f1aa6089ae904e2ec03c087
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-26 16:07:22 -0500

    When steps equals 0-1, only one way to reach top

--- staircase.rb
+++ staircase.rb
@@ -3,6 +3,12 @@ p "Staircase!"
 # Function to take in two Parameters
 # n represents the amount of steps on the stair case 
 # storage is in array that stores results to prevent redundant computation
+# Function is recursive: Much like Fibbonacci sequence
 # Returns number of ways to reach top of staircase
 def stair_case n, storage
+	# Exit code
+	# When steps equals 0-1, only one way to reach top
+	if n <= 1
+		return 1
+	end	
 end	
