103e53318810439e8032ab3c3b4284a42e401314
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-26 16:05:53 -0500

    Defined function

--- staircase.rb
+++ staircase.rb
@@ -1,1 +1,8 @@
 p "Staircase!"
+
+# Function to take in two Parameters
+# n represents the amount of steps on the stair case 
+# storage is in array that stores results to prevent redundant computation
+# Returns number of ways to reach top of staircase
+def stair_case n, storage
+end	
