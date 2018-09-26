273ffb50fdc279d7c2b7ce6bf95911cb866dc650
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-26 16:14:15 -0500

    Defualt value of storage is array so user won't have to pass it in

--- staircase.rb
+++ staircase.rb
@@ -2,10 +2,10 @@ p "Staircase!"
 
 # Function to take in two Parameters
 # n represents the amount of steps on the stair case 
-# storage is in array that stores results to prevent redundant computation
+# storage is in array that stores results to prevent redundant computation (Defualt value is array)
 # Function is recursive: Much like Fibbonacci sequence
 # Returns number of ways to reach top of staircase
-def stair_case n, storage
+def stair_case n, storage = []
 	# Exit code
 	# When steps equals 0-1, only one way to reach top
 	if n <= 1
